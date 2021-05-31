0acebe62be7cef9bcb29bde4a95bc904336a2f39
MahdiehSedaghat <mahdie.sedaghat@gmail.com>
2021-06-01 04:27:39 +0430

    create reusable app component

---
+++ app/components/AppButton/AppButton.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import defaultStyles from '../../config/styles';
+
+interface IProps {
+  title: string;
+  onPress: any;
+  color?: 'primary' | 'secondary';
+  disabled?: boolean;
+}
+
+function AppButton({title, onPress, color = 'primary', disabled}: IProps) {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.button,
+        {
+          backgroundColor: defaultStyles.colors[color],
+        },
+      ]}
+      onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: defaultStyles.colors.primary,
+    borderRadius: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    width: '100%',
+  },
+  text: {
+    color: defaultStyles.colors.white,
+    fontSize: 14,
+    fontWeight: '600',
+  },
+});
+
+export default AppButton;
